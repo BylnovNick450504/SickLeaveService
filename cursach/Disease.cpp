9a0e760547e1ff14d5560ff567cadf4113f8561f
BylnovNick450504 <bylnov.nick@gmail.com>
2019-04-21 16:57:20 +0300

    create base structure Disease class

--- cursach/Disease.cpp
+++ cursach/Disease.cpp
@@ -1,1 +1,24 @@
 #include "pch.h"
+
+using namespace std;
+
+Disease::Disease(int id, string name, int diseaseCode) : AbstractEntity(id) {
+	this->name = name;
+	this->diseaseCode = diseaseCode;
+}
+
+string Disease::getName() {
+	return name;
+}
+
+void Disease::setName(string name) {
+	this->name = name;
+}
+
+int Disease::getDiseaseCode() {
+	return diseaseCode;
+}
+
+void Disease::setDiseaseCode(int diseaseCode) {
+	this->diseaseCode = diseaseCode;
+}
--- cursach/Disease.h
+++ cursach/Disease.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "pch.h"
+#include "AbstractEntity.h"
+#include <iostream>
+#include <string>
+
+class Disease : public AbstractEntity {
+private:
+	std::string name;
+	int diseaseCode;
+public:
+	Disease(int id, std::string name, int diseaseCode);
+	std::string getName();
+	void setName(std::string name);
+	int getDiseaseCode();
+	void setDiseaseCode(int diseaseCode);
+};
