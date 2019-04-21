c8dbeedc66cf15b10c1fb890a756616acb01b1c9
BylnovNick450504 <bylnov.nick@gmail.com>
2019-04-21 16:55:58 +0300

    make base structure Patient class

--- cursach/Patient.cpp
+++ cursach/Patient.cpp
@@ -1,1 +1,21 @@
 #include "pch.h"
+
+using namespace std;
+
+Patient::Patient(int id, string name) : Human(id, name) {}
+
+string Patient::getCardNumber() {
+	return cardNumber;
+}
+
+void Patient::setCardNumber(string cardNumber) {
+	this->cardNumber = cardNumber;
+}
+
+string Patient::getPassportNumber() {
+	return pasportNumber;
+}
+
+void Patient::setPassportNumber(string passportNumber) {
+	this->pasportNumber = passportNumber;
+}
--- cursach/Patient.h
+++ cursach/Patient.h
@@ -1,8 +1,17 @@
 #pragma once
 #include "pch.h"
 #include <iostream>
-#include "AbstractEntity.h"
+#include "Human.h"
 
-class Patient: public AbstractEntity {
+class Patient: public Human {
+private:
+	std::string cardNumber;
+	std::string pasportNumber;
+public:
+	Patient(int id, std::string name);
 
+	std::string getCardNumber();
+	void setCardNumber(std::string cardNumber);
+	std::string getPassportNumber();
+	void setPassportNumber(std::string passportNumber);
 };
