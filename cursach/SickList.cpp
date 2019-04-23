d1a07edfdfddb72d10c599d2f96c1267bbc23c98
BylnovNick450504 <bylnov.nick@gmail.com>
2019-04-23 14:48:39 +0300

    create basic structure SickList

--- cursach/SickList.cpp
+++ cursach/SickList.cpp
@@ -1,1 +1,53 @@
 #include "pch.h"
+
+using namespace std;
+
+SickList::SickList(int id) : AbstractEntity(id) {}
+
+int SickList::getDoctorId() {
+	return doctorId;
+}
+
+void SickList::setDoctorId(int doctorId) {
+	this->doctorId = doctorId;
+}
+
+int SickList::getPatientId() {
+	return patientId;
+}
+
+void SickList::setPatientId(int patientId) {
+	this->patientId = patientId;
+}
+
+int SickList::getDiseaseId() {
+	return diseaseId;
+}
+
+void SickList::setDiseaseId(int diseaseId) {
+	this->diseaseId = diseaseId;
+}
+
+tm SickList::getStartDate() {
+	return startDate;
+}
+
+void SickList::setStartDate(tm startDate) {
+	this->startDate = startDate;
+}
+
+tm SickList::getDueDate() {
+	return dueDate;
+}
+
+void SickList::setDueDate(tm dueDate) {
+	this->dueDate = dueDate;
+}
+
+string SickList::getDescription() {
+	return description;
+}
+
+void SickList::setDescription(string description) {
+	this->description = description;
+}
--- cursach/SickList.h
+++ cursach/SickList.h
@@ -8,4 +8,28 @@ class SickList : public AbstractEntity {
 private: 
 	int doctorId;
 	int patientId;
+	int diseaseId;
+	tm startDate;
+	tm dueDate;
+	std::string description;
+public:
+	SickList(int id);
+	
+	int getDoctorId();
+	void setDoctorId(int doctorId);
+	
+	int getPatientId();
+	void setPatientId(int patientId);
+
+	int getDiseaseId();
+	void setDiseaseId(int diseaseId);
+
+	tm getStartDate();
+	void setStartDate(tm startDate);
+
+	tm getDueDate();
+	void setDueDate(tm dueDate);
+
+	std::string getDescription();
+	void setDescription(std::string description);
 };
